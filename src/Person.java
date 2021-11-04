2759eb7c5e53e386dfc62042862b40bad314162b
Ivanova Irina <ivanovairishav@ya.ru>
2021-11-04 14:56:12 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,96 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean hasAge() {
+        if(OptionalInt.of(age).isEmpty())
+            return false;
+        else
+            return true;
+    }
+
+    public boolean hasAddress() {
+        if(getAddress() != null)
+            return true;
+        else
+            setAddress(address);
+        return false;
+    }
+
+    public String setAddress(String address) {
+        this.address = address;
+        return address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge())
+            age = age + 1;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(getSurname()).setAddress(getAddress());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+}
+
